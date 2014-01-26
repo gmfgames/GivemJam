208e3671e02f8841f10fc8c1c1177aca4b31cfa7
thomazpadilha <padilha.thomaz@gmail.com>
2014-01-26 01:17:27 -0200

    Exclusão das Demos Alteração de plataforma (Android) Atualização do PlayerBubbleEmitter Criação dos prefabs de bolhas

--- Assets/Scripts/com/gmf/givemejam/fx/PlayerBubbleEmitter.cs
+++ Assets/Scripts/com/gmf/givemejam/fx/PlayerBubbleEmitter.cs
@@ -5,42 +5,24 @@ using TouchScript.Gestures;
 [RequireComponent(typeof(PlayerMove))]
 public class PlayerBubbleEmitter : MonoBehaviourExtends {
 
-	public float threshould = .3f;
 	public ParticleSystem emitterLeft;
 	public ParticleSystem emitterRight;
-
-	public TapGesture leftButton;
-	public TapGesture rightButton;
 
 	public override void onPause(bool isPaused){
 	
 	}
-	
-	// Use this for initialization
-	void Awake () {
-		if(leftButton){
-			leftButton.StateChanged += LeftButtonPress;
-		}else
-			throw new UnityException("O botao esquerdo de movimentaçao do personagem nao esta configurado.");
-		
-		if(rightButton){
-			rightButton.StateChanged += RightButtonPress;
 
-		}else
-			throw new UnityException("O botao direito de movimentaçao do personagem nao esta configurado.");
-	}
-
-	void LeftButtonPress (object sender, TouchScript.Events.GestureStateChangeEventArgs e)
+	public void LeftEmit ()
 	{
-		if(!isPaused && isEnabled && e.State == Gesture.GestureState.Began){
+		if(!isPaused && isEnabled){
 			ParticleSystem particle = Instantiate(emitterRight, transform.position, Quaternion.identity) as ParticleSystem;
 			Destroy(particle.gameObject, emitterLeft.duration);
 		}
 	}
 	
-	void RightButtonPress (object sender, TouchScript.Events.GestureStateChangeEventArgs e)
+	public void RightEmit ()
 	{
-		if(!isPaused && isEnabled && e.State == Gesture.GestureState.Began){
+		if(!isPaused && isEnabled){
 			ParticleSystem particle = Instantiate(emitterLeft, transform.position, Quaternion.identity) as ParticleSystem;
 			Destroy(particle.gameObject, emitterRight.duration);
 		}
